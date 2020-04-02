ebf35a5987507bd75f1deb49af717d0ab9de0bd6
erikarberry <erikarberry@gmail.com>
2020-04-01 23:54:06 -0400

    final push

--- static/js/logic.js
+++ static/js/logic.js
@@ -11,49 +11,57 @@ function createFeatures(earthquakeData) {
 // Define a function we want to run once for each feature in the features array
 // Give each feature a popup describing the place and time of the earthquake  
   function onEachFeature(feature, layer) {
-      layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
-        "</h3><hr><p>Magnitude: " + feature.properties.mag + "</p>");
-    }
+    layer.bindPopup(
+      "<h3>" + 
+      feature.properties.place +
+      "</h3><hr><p>" + 
+      new Date(feature.properties.time) + 
+      "</p>" +
+      "</h3><hr><p>Magnitude: " + 
+      feature.properties.mag + 
+      "</p>"
+    );
+  }
+  var colorScaleRange = ['#ffffb2', '#bd0026']
+  var minmax = d3.extent(earthquakeData.map((feature) => feature.properties.mag));
+  var colorScale = d3.scaleLinear().domain(minmax).range(colorScaleRange);
 
 // Create a GeoJSON layer containing the features array on the earthquakeData object
 // Run the onEachFeature function once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
         onEachFeature: onEachFeature,
         pointToLayer: function (feature, latlng) {
-          var colorScaleRange = ['#ffffb2', '#bd0026']
-          var minmax = d3.extent(earthquakeData.map((feature) => feature.properties.mag));
-          var range = [0, earthquakeData.length - 1];
-          var domain = [1,minmax[1]];
-          var bins = d3.ticks(Math.floor(minmax[0]), minmax[1], Math.ceil(minmax[1] - minmax[0]));
-          var colorScale = d3.scaleLinear().domain(minmax).range(colorScaleRange);
-          var getColor  = d3.scaleLinear().domain(domain).range(colorScale);
-      
           var geojsonMarkerOptions = {
-            radius: 2*feature.properties.mag,
+            radius: Math.pow(2, feature.properties.mag - 1),
             fillColor: colorScale(+feature.properties.mag),
             color: "#000",
             weight: 1,
             opacity: 1,
             fillOpacity: 0.8
-            };
+          };
           return L.circleMarker(latlng, geojsonMarkerOptions);
-          }
-  });
+        }
+    });
+    var bins = d3.ticks(Math.floor(minmax[0]), minmax[1], Math.ceil(minmax[1] - minmax[0]));
+    var legend = {
+      limits: bins,
+      colors: bins.map(mag => colorScale(mag))
+    }
 
 // Sending our earthquakes layer to the createMap function
-createMap(earthquakes);
+createMap(earthquakes, legend);
 }
 
-function createMap(earthquakes) {
+function createMap(earthquakes, legendOptions) {
 
 // Define streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.streets",
-      accessToken: API_KEY
-  });
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+    }
+  );
   
 // Define a baseMaps object to hold our base layers
   var baseMaps = {
@@ -67,43 +75,38 @@ function createMap(earthquakes) {
 
 // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
-      center: [
-        37.09, -95.71
-      ],
+      center: [37.09, -95.71],
       zoom: 5,
       layers: [streetmap, earthquakes]
   });
 
-// function getColor(d) {
-//     return d < 1 ? 'rgb(255,255,178)' :
-//         d < 2  ? 'rgb(254,217,118)' :
-//         d < 3  ? 'rgb(254,178,76)' :
-//         d < 4  ? 'rgb(253,141,60)' :
-//         d < 5  ? 'rgb(240,59,32)' :
-//         d < 6  ? 'rgb(189,0,38)' :
-//                     'rgb(255,0,0)';
-// }
-
-// // // Create a legend to display information about our map
-// var legend = L.control({position: 'bottomright'});
-// legend.onAdd = function () {
-//     var div = L.DomUtil.create('div', 'info legend'),
-//     bins = [0, 1, 2, 3, 4, 5],
-//     labels = [];
-
-//     div.innerHTML+='Magnitude<br><hr>'
-
-//     // loop through our density intervals and generate a label with a colored square for each interval
-//     for (var i = 0; i < bins.length; i++) {
-//         div.innerHTML +=
-//             '<i style="background:' + getColor(bins[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
-//             bins[i] + (bins[i + 1] ? '&ndash;' + bins[i + 1] + '<br>' : '+');
-//     }
-// }
-};
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
   // Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+      collapsed: false
+    }).addTo(myMap);
+
+// // Create a legend to display information about our map
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function () {
+    var div = L.DomUtil.create('div', 'info legend');
+    var limits = legendOptions.limits;
+    var colors = legendOptions.colors;
+    var labels = [];
+
+    // Add min & max
+    var legendInfo = `
+      <h1>Magnitude</h1>`;
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+      labels.push(`<li style="background-color:${colors[index]};">${limit}</li>`);
+    });
+
+    div.innerHTML += `<ul style="">${labels.join("")}</ul>`
+    return div;
+  };
+  legend.addTo(myMap);
+}
--- static/js/logicTest.js
+++ static/js/logicTest.js
@@ -11,49 +11,57 @@ function createFeatures(earthquakeData) {
 // Define a function we want to run once for each feature in the features array
 // Give each feature a popup describing the place and time of the earthquake  
   function onEachFeature(feature, layer) {
-      layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
-        "</h3><hr><p>Magnitude: " + feature.properties.mag + "</p>");
-    }
+    layer.bindPopup(
+      "<h3>" + 
+      feature.properties.place +
+      "</h3><hr><p>" + 
+      new Date(feature.properties.time) + 
+      "</p>" +
+      "</h3><hr><p>Magnitude: " + 
+      feature.properties.mag + 
+      "</p>"
+    );
+  }
+  var colorScaleRange = ['#ffffb2', '#bd0026']
+  var minmax = d3.extent(earthquakeData.map((feature) => feature.properties.mag));
+  var colorScale = d3.scaleLinear().domain(minmax).range(colorScaleRange);
 
 // Create a GeoJSON layer containing the features array on the earthquakeData object
 // Run the onEachFeature function once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
         onEachFeature: onEachFeature,
         pointToLayer: function (feature, latlng) {
-          var colorScaleRange = ['#ffffb2', '#bd0026']
-          var minmax = d3.extent(earthquakeData.map((feature) => feature.properties.mag));
-          var range = [0, earthquakeData.length - 1];
-          var domain = [1,minmax[1]];
-          // var bins = d3.ticks(Math.floor(minmax[0]), minmax[1], Math.ceil(minmax[1] - minmax[0]));
-          var colorScale = d3.scaleLinear().domain(minmax).range(colorScaleRange);
-          // var getColor  = d3.scaleLinear().domain(domain).range(colorScale);
-      
           var geojsonMarkerOptions = {
-            radius: 2*feature.properties.mag,
+            radius: Math.pow(2, feature.properties.mag - 1),
             fillColor: colorScale(+feature.properties.mag),
             color: "#000",
             weight: 1,
             opacity: 1,
             fillOpacity: 0.8
-            };
+          };
           return L.circleMarker(latlng, geojsonMarkerOptions);
-          }
-  });
+        }
+    });
+    var bins = d3.ticks(Math.floor(minmax[0]), minmax[1], Math.ceil(minmax[1] - minmax[0]));
+    var legend = {
+      limits: bins,
+      colors: bins.map(mag => colorScale(mag))
+    }
 
 // Sending our earthquakes layer to the createMap function
-createMap(earthquakes);
+createMap(earthquakes, legend);
 }
 
-function createMap(earthquakes) {
+function createMap(earthquakes, legendOptions) {
 
 // Define streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.streets",
-      accessToken: API_KEY
-  });
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+    }
+  );
   
 // Define a baseMaps object to hold our base layers
   var baseMaps = {
@@ -67,9 +75,7 @@ function createMap(earthquakes) {
 
 // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
-      center: [
-        37.09, -95.71
-      ],
+      center: [37.09, -95.71],
       zoom: 5,
       layers: [streetmap, earthquakes]
   });
@@ -78,42 +84,29 @@ function createMap(earthquakes) {
   // Pass in our baseMaps and overlayMaps
   // Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-
-// function getColor(d) {
-//     return d < 1 ? 'rgb(255,255,178)' :
-//         d < 2  ? 'rgb(254,217,118)' :
-//         d < 3  ? 'rgb(254,178,76)' :
-//         d < 4  ? 'rgb(253,141,60)' :
-//         d < 5  ? 'rgb(240,59,32)' :
-//         d < 6  ? 'rgb(189,0,38)' :
-//                     'rgb(255,0,0)';
-// }
+      collapsed: false
+    }).addTo(myMap);
 
 // // Create a legend to display information about our map
 var legend = L.control({position: 'bottomright'});
 legend.onAdd = function () {
     var div = L.DomUtil.create('div', 'info legend');
-    var limits = geojson.options.limits;
-    var colors = geojson.options.colors;
+    var limits = legendOptions.limits;
+    var colors = legendOptions.colors;
     var labels = [];
 
     // Add min & max
-    var legendInfo = "<h1>Magnitude</h1>" +
-      "<div class=\"labels\">" +
-        "<div class=\"min\">" + limits[0] + "</div>" +
-        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-      "</div>";
+    var legendInfo = `
+      <h1>Magnitude</h1>`;
 
     div.innerHTML = legendInfo;
 
     limits.forEach(function(limit, index) {
-      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+      labels.push(`<li style="background-color:${colors[index]};">${limit}</li>`);
     });
 
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    div.innerHTML += `<ul style="">${labels.join("")}</ul>`
     return div;
   };
   legend.addTo(myMap);
-};
+}
